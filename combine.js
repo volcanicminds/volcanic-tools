a8eb2c632fff9909db0bc0d7cf9793ecf103c339
Davide Morra <davide.morra@volcanicminds.com>
2025-12-01 09:54:07 +0100

    Change title

--- combine.js
+++ combine.js
@@ -5,7 +5,7 @@ const MAIN_DIRECTORY = './'
 const INCLUDE_FILES = ['package.json', 'tsconfig.json', 'README.md', '.nvmrc', 'index.ts', 'index.d.ts']
 const SUB_DIRECTORIES = ['lib', 'types']
 const OUTPUT_FILE = 'OUTPUT.md'
-const CUSTOM_TITLE = 'Volcanic Tools'
+const CUSTOM_TITLE = '@volcanicminds/tools'
 
 /**
  * Recursively collects .js and .ts files from a given directory.
